8a079c66d11c670edf6c9f4b2fcbcf5ac178f713
AdnanMadi417 <164775717+AdnanMadi417@users.noreply.github.com>
2024-12-31 18:36:25 +0800

    added student field to the back end

--- requests/models.py
+++ requests/models.py
@@ -4,7 +4,7 @@ from students.models import Student
 
 class MaintenanceRequest(models.Model):
     STATUS_CHOICES = (
-        ('appending', 'Appending'),
+        ('pending', 'Pending'),
         ('ppk_done', 'PPK Done'),
         ('rejected', 'Rejected'),
     )
@@ -15,6 +15,7 @@ class MaintenanceRequest(models.Model):
     )
 
     student_id = models.CharField(max_length=20, default='')
+    student = models.CharField(max_length=50, default='')
     phone = models.CharField(max_length=15, default='')
     email = models.EmailField(default='')
     gender = models.CharField(max_length=10, choices=GENDER_CHOICES, default='male')
@@ -24,7 +25,7 @@ class MaintenanceRequest(models.Model):
     occurrence = models.CharField(max_length=255)
     evidence_photo = models.FileField(upload_to='maintenance_evidence/', default='null')
     explanation = models.TextField()
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='appending')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Pending')
 
     def __str__(self):
         return f'Request by {self.student_id}'
@@ -43,6 +44,7 @@ class ChangeRoomRequest(models.Model):
     )
 
     student_id = models.CharField(max_length=20, default='')
+    student = models.CharField(max_length=50, default='')
     phone = models.CharField(max_length=15, default='')
     email = models.EmailField(default='')
     gender = models.CharField(max_length=10, choices=GENDER_CHOICES, default='male')
@@ -50,7 +52,7 @@ class ChangeRoomRequest(models.Model):
     room_number = models.CharField(max_length=10)
     supporting_doc = models.FileField(upload_to='change_room_docs/')
     reason = models.TextField()
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES , default = 'appending')
 
     def __str__(self):
         return f'Change Room Request by {self.student_id}'
