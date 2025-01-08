8d7a517e6cb87c4da58b7bac4829f2907d8a2176
YounesDjelloul <younesdjelloul14@gmail.com>
2025-01-08 17:43:13 +0800

    flattening obj

--- students/serializers.py
+++ students/serializers.py
@@ -5,8 +5,39 @@ from .models import Student
 
 
 class StudentSerializer(serializers.ModelSerializer):
-    bed = BedSerializer(read_only=True)
+    # Add all the bed-related fields directly to the student serializer
+    bed_id = serializers.IntegerField(source='bed.id', read_only=True)
+    bed_number = serializers.CharField(source='bed.bed_number', read_only=True)
+    bed_status = serializers.CharField(source='bed.current_status', read_only=True)
+    room_id = serializers.IntegerField(source='bed.room.id', read_only=True)
+    room_number = serializers.CharField(source='bed.room.number', read_only=True)
+    level_id = serializers.IntegerField(source='bed.room.level.id', read_only=True)
+    level_number = serializers.IntegerField(source='bed.room.level.number', read_only=True)
+    hostel_id = serializers.IntegerField(source='bed.room.level.hostel.id', read_only=True)
+    hostel_name = serializers.CharField(source='bed.room.level.hostel.name', read_only=True)
 
     class Meta:
         model = Student
-        fields = '__all__'
+        fields = [
+            'id',
+            'name',
+            'student_id',
+            'passport',
+            'arrival_date',
+            'phone',
+            'email',
+            'gender',
+            'religion',
+            'nationality',
+            'major',
+            'status',
+            'bed_id',
+            'bed_number',
+            'bed_status',
+            'room_id',
+            'room_number',
+            'level_id',
+            'level_number',
+            'hostel_id',
+            'hostel_name',
+        ]
