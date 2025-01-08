89aadb4c5944252ac64a393ac62ba59279d49275
YounesDjelloul <younesdjelloul14@gmail.com>
2025-01-08 15:16:46 +0800

    including bed details in student details

--- students/serializers.py
+++ students/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
+
+from hostels.serializers import BedSerializer
 from .models import Student
 
 class StudentSerializer(serializers.ModelSerializer):
+    bed = BedSerializer()
     class Meta:
         model = Student
         fields = '__all__'
