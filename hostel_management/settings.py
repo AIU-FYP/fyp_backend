12b05c599e468fbee4c2247ae1f05a2b59b296ae
adnanmadi417 <uso53dr6i8s4yt0idkq7f6ivj04en8@bots.bitbucket.org>
2025-07-07 12:28:51 +0800

    still iam doing debugging database url 3 changed link

--- hostel_management/settings.py
+++ hostel_management/settings.py
@@ -98,12 +98,9 @@ WSGI_APPLICATION = 'hostel_management.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True
-    )
+    'default': dj_database_url.config(conn_max_age=600)
 }
+
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
